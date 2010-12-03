22225e8c010f8c259f1ee750c94369ae7e0a3d20
mmakowski <maciek.makowski@gmail.com>
2010-12-03 15:08:09 +0000

    set up cabal

---
+++ src/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+import Graphics.UI.WX
+
+startWidth, startHeight :: Int
+startWidth = 400
+startHeight = 300
+
+main :: IO ()
+main = start habazWindow
+
+habazWindow :: IO ()
+habazWindow =
+  do f <- frame [text := "Habaz"]
+     p <- panel f [on paint := paintBoard]
+     set f [layout := minsize (sz startWidth startHeight) $ widget p]
+       
+paintBoard :: DC a -> Rect -> IO ()
+paintBoard dc viewArea = 
+  do set dc [brushColor := red, brushKind := BrushSolid]
+     polygon dc [(Point 0 0), (Point 0 20), (Point 50 10)] []
