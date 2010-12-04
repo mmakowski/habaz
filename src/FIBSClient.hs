ba586a61793210db1d669a67273b7cc2abeaf31d
Maciek Makowski <maciek.makowski@gmail.com>
2010-12-04 13:45:38 +0000

    added read test

--- src/FIBSClient.hs
+++ src/FIBSClient.hs
@@ -1,17 +1,19 @@
 module FIBSClient where 
+import Char
+import Control.Monad
 import Data.Bits
-import Network.Socket
-import Network.BSD
 import Data.List
+import Network.Socket hiding (connect)
+import qualified Network.Socket (connect)
+import Network.BSD
 import System.IO
 
-defaultFibsHost :: HostName
-defaultFibsHost = "fibs.com"
-defaultFibsPort :: String
-defaultFibsPort = "4321"
 
 type Command = String
 type Connection = Handle
+
+defaultFibsHost = "fibs.com"
+defaultFibsPort = "4321"
 
 connect :: HostName             -- ^ The host to connect to
         -> String               -- ^ Port number
@@ -22,7 +24,7 @@ connect hostname port =
      sock <- socket (addrFamily serveraddr) Stream defaultProtocol
      setSocketOption sock KeepAlive 1
      Network.Socket.connect sock (addrAddress serveraddr)
-     conn <- socketToHandle sock WriteMode
+     conn <- socketToHandle sock ReadWriteMode
      return conn
 
 -- login :: Handle
@@ -35,3 +37,17 @@ send conn cmd =
 
 disconnect :: Connection -> IO ()
 disconnect conn = hClose conn
+
+test = 
+  do conn <- connect defaultFibsHost defaultFibsPort
+     readUntil '&' conn
+     return $ disconnect conn
+
+-- TODO: termStr should be a string; check if termStr is matched as the character are read
+readUntil termStr conn = liftM reverse $ loop []
+  where
+    loop acc = do
+      input <- hGetChar conn --fibsGetLine conn
+      putStr $ [input] ++ ":" ++ (show $ Char.ord input) ++ " "
+      (if input /= termStr then loop else return) (input:acc)
+      
