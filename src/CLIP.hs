35361c4e4aadcd5d1427ff423807f79dd26da69a
Maciek Makowski <maciek.makowski@gmail.com>
2011-02-15 18:27:54 +0000

    completed OwnInfo parsing

--- src/CLIP.hs
+++ src/CLIP.hs
@@ -1,6 +1,7 @@
 module CLIP(
   CLIPMessage (..), 
   ParseResult (Success, Failure),
+  RedoubleLimit (..),
   parseMessage,
   parseMessages
   ) where
@@ -8,6 +9,12 @@ import Control.Applicative
 import Data.List
 import Data.Time
 import System.Locale
+
+-- TODO: move to a separate Backgammon module
+data RedoubleLimit
+     = LimitedTo Int
+     | Unlimited
+     deriving (Eq, Show)
 
 data CLIPMessage 
      = FailedLogin
@@ -19,7 +26,23 @@ data CLIPMessage
                , allowPip :: Bool 
                , autoBoard :: Bool 
                , autoDouble :: Bool 
-               , autoMove :: Bool       
+               , autoMove :: Bool
+               , away :: Bool
+               , bell :: Bool
+               , crawford :: Bool
+               , double :: Bool
+               , experience :: Int
+               , greedy :: Bool
+               , moreBoards :: Bool
+               , moves :: Bool
+               , notify :: Bool
+               , rating :: Float
+               , ratings :: Bool
+               , ready :: Bool
+               , redoubles :: RedoubleLimit
+               , report :: Bool
+               , silent :: Bool
+               , timeZone :: String
                }
      deriving (Eq, Show)
 
@@ -72,7 +95,23 @@ parseLine ["2", name, allowpip, autoboard, autodouble, automove, away, bell, cra
                parseBool allowpip <*> 
                parseBool autoboard <*> 
                parseBool autodouble <*>
-               parseBool automove,
+               parseBool automove <*>
+               parseBool away <*>
+               parseBool bell <*>
+               parseBool crawford <*>
+               parseBool double <*>
+               parse experience <*>
+               parseBool greedy <*>
+               parseBool moreboards <*>
+               parseBool moves <*>
+               parseBool notify <*>
+               parse rating <*>
+               parseBool ratings <*>
+               parseBool ready <*>
+               parseRedoubleLimit redoubles <*>
+               parseBool report <*>
+               parseBool silent <*>
+               pure timezone,
    rest)
 -- TODO: other cases
 parseLine words rest = (Failure $ "unable to parse: " ++ (unwords words), rest)
@@ -80,6 +119,11 @@ parseLine words rest = (Failure $ "unable to parse: " ++ (unwords words), rest)
 strs `containsPrefixOf` str = foldl (\b s -> b || s `isPrefixOf` str) False strs
 
 lineTerminators = ["\n", "login:"]
+
+parse :: Read a => String -> ParseResult a
+parse str = case reads str of
+  [(val, "")] -> Success val
+  _ -> Failure $ "unable to parse '" ++ str ++ "'"
 
 parseUTCTime :: String -> ParseResult UTCTime
 parseUTCTime str = case parseTime defaultTimeLocale "%s" str of
@@ -90,4 +134,10 @@ parseBool :: String -> ParseResult Bool
 parseBool "1" = Success True
 parseBool "0" = Success False
 parseBool str = Failure $ "unable to parse '" ++ str ++ "' as Bool"
+
+parseRedoubleLimit :: String -> ParseResult RedoubleLimit
+parseRedoubleLimit "unlimited" = Success Unlimited
+parseRedoubleLimit str = case reads str of
+  [(num, "")] -> Success $ LimitedTo num
+  _ -> Failure $ "unable to parse '" ++ str ++"' as redoubles limit"
 
--- test/CLIP.hs
+++ test/CLIP.hs
@@ -11,9 +11,14 @@ test_parseMessage_failedLoginParsedCorrectly =
               (parseMessage "login:")
 
 test_parseMessage_welcomeParsedCorrectly = 
-  assertEqual "welcome message"
+  assertEqual "CLIP Welcome"
               (Success (Welcome "username" (fromJust $ parseTime defaultTimeLocale "%s" "1041253132") "1.2.3.4"), [])
               (parseMessage "1 username 1041253132 1.2.3.4")
+
+test_parseMessage_ownInfoParsedCorrectly = 
+  assertEqual "CLIP Own Info"
+              (Success (OwnInfo "myself" True True False False False False True True 2396 False True False True 3457.85 False False (LimitedTo 0) False False "Australia/Melbourne"), [])
+              (parseMessage "2 myself 1 1 0 0 0 0 1 1 2396 0 1 0 1 3457.85 0 0 0 0 0 Australia/Melbourne")
 
 test_parseMessages_isLazy = 
   assertEqual "parseMessages is lazy" 
