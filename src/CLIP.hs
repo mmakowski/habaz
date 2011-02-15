a8d7996efc18530862c1b1874496a74257c32e74
Maciek Makowski <maciek.makowski@gmail.com>
2011-02-15 22:42:46 +0000

    implemented parsing of MOTD and Who Info

--- src/CLIP.hs
+++ src/CLIP.hs
@@ -1,6 +1,7 @@
 module CLIP(
   CLIPMessage (..), 
   ParseResult (Success, Failure),
+  PlayerInfo (..),
   RedoubleLimit (..),
   parseMessage,
   parseMessages
@@ -14,6 +15,22 @@ import System.Locale
 data RedoubleLimit
      = LimitedTo Int
      | Unlimited
+     deriving (Eq, Show)
+
+data PlayerInfo 
+     = PlayerInfo { pName :: String
+                  , pOpponent :: Maybe String
+                  , pWatching :: Maybe String
+                  , pReady :: Bool
+                  , pAway :: Bool
+                  , pRating :: Float
+                  , pExperience :: Int
+                  , pIdle :: Int
+                  , pLogin :: UTCTime
+                  , pHostName :: String
+                  , pClient :: Maybe String
+                  , pEmail :: Maybe String
+                  }
      deriving (Eq, Show)
 
 data CLIPMessage 
@@ -44,6 +61,8 @@ data CLIPMessage
                , silent :: Bool
                , timeZone :: String
                }
+     | MOTD String
+     | WhoInfo [PlayerInfo]
      deriving (Eq, Show)
 
 data ParseResult a 
@@ -70,16 +89,8 @@ parseMessages str =
 
 parseMessage :: String -> (ParseResult CLIPMessage, String)
 parseMessage str = 
-  let (first, rest) = firstLine str 
+  let (first, rest) = firstLineAndRest str 
   in parseLine (words first) rest
-  where
-    firstLine str = 
-      let (revFirst, rest) = loop [] str in (reverse revFirst, rest)
-    rTermStrs = map reverse lineTerminators
-    loop acc [] = (acc, [])
-    loop acc (h:t) = 
-      let currStr = (h:acc) 
-      in if rTermStrs `containsPrefixOf` currStr then (currStr, t) else loop currStr t
 
 parseLine :: [String] -> String -> (ParseResult CLIPMessage, String)
 -- failed login
@@ -91,30 +102,77 @@ parseLine ["1", username, millisString, ip] rest =
 parseLine ["2", name, allowpip, autoboard, autodouble, automove, away, bell, crawford, 
            double, experience, greedy, moreboards, moves, notify, rating, ratings, ready, 
            redoubles, report, silent, timezone] rest = 
-  (OwnInfo <$> pure name <*> 
-               parseBool allowpip <*> 
-               parseBool autoboard <*> 
-               parseBool autodouble <*>
-               parseBool automove <*>
-               parseBool away <*>
-               parseBool bell <*>
-               parseBool crawford <*>
-               parseBool double <*>
-               parse experience <*>
-               parseBool greedy <*>
-               parseBool moreboards <*>
-               parseBool moves <*>
-               parseBool notify <*>
-               parse rating <*>
-               parseBool ratings <*>
-               parseBool ready <*>
-               parseRedoubleLimit redoubles <*>
-               parseBool report <*>
-               parseBool silent <*>
-               pure timezone,
+  (OwnInfo <$> pure name 
+           <*> parseBool allowpip 
+           <*> parseBool autoboard 
+           <*> parseBool autodouble
+           <*> parseBool automove 
+           <*> parseBool away
+           <*> parseBool bell
+           <*> parseBool crawford
+           <*> parseBool double
+           <*> parse experience
+           <*> parseBool greedy
+           <*> parseBool moreboards
+           <*> parseBool moves
+           <*> parseBool notify
+           <*> parse rating
+           <*> parseBool ratings
+           <*> parseBool ready
+           <*> parseRedoubleLimit redoubles
+           <*> parseBool report
+           <*> parseBool silent
+           <*> pure timezone,
    rest)
+-- CLIP MOTD
+parseLine ["3"] rest = 
+  let (motd, rest') = readMOTD "" rest
+  in (Success $ MOTD motd, rest')
+  where
+    readMOTD acc str = 
+      let (first, rest) = firstLineAndRest str
+      in case words first of 
+        ["4"] -> (acc, rest)
+        _ -> readMOTD (acc ++ first) rest
+-- CLIP Who Info
+parseLine line@("5" : _) rest =
+  let (infos, rest') = parseWhoInfo [] line rest
+  in (WhoInfo <$> lift infos, rest')
+  where 
+    parseWhoInfo acc ["6"] rest = (acc, rest)
+    parseWhoInfo acc line@("5" : _) rest = 
+      let (next, rest') = firstLineAndRest rest
+      in parseWhoInfo (acc ++ [parsePlayerInfo line]) (words next) rest'
+    lift [] = pure []
+    lift (pr:prs) = (:) <$> pr <*> lift prs
 -- TODO: other cases
 parseLine words rest = (Failure $ "unable to parse: " ++ (unwords words), rest)
+
+parsePlayerInfo ["5", name, opponent, watching, ready, away, rating, experience, idle, 
+                 login, hostname, client, email] = 
+  PlayerInfo <$> pure name
+             <*> parseMaybeString opponent
+             <*> parseMaybeString watching
+             <*> parseBool ready
+             <*> parseBool away
+             <*> parse rating
+             <*> parse experience
+             <*> parse idle
+             <*> parseUTCTime login
+             <*> pure hostname
+             <*> parseMaybeString client
+             <*> parseMaybeString email
+parsePlayerInfo w = Failure $ "unable to parse " ++ (show w) ++ " as player info"
+
+firstLineAndRest :: String -> (String, String)
+firstLineAndRest str = 
+  let (revFirst, rest) = loop [] str in (reverse revFirst, rest)
+  where
+    rTermStrs = map reverse lineTerminators
+    loop acc [] = (acc, [])
+    loop acc (h:t) = 
+      let currStr = (h:acc) 
+      in if rTermStrs `containsPrefixOf` currStr then (currStr, t) else loop currStr t
 
 strs `containsPrefixOf` str = foldl (\b s -> b || s `isPrefixOf` str) False strs
 
@@ -125,19 +183,23 @@ parse str = case reads str of
   [(val, "")] -> Success val
   _ -> Failure $ "unable to parse '" ++ str ++ "'"
 
-parseUTCTime :: String -> ParseResult UTCTime
-parseUTCTime str = case parseTime defaultTimeLocale "%s" str of
-  Just time -> Success time
-  Nothing -> Failure $ "unable to parse '" ++ str ++ "' as UTCTime"
-
 parseBool :: String -> ParseResult Bool
 parseBool "1" = Success True
 parseBool "0" = Success False
 parseBool str = Failure $ "unable to parse '" ++ str ++ "' as Bool"
+
+parseMaybeString :: String -> ParseResult (Maybe String)
+parseMaybeString "-" = Success Nothing
+parseMaybeString str = Success (Just str)
 
 parseRedoubleLimit :: String -> ParseResult RedoubleLimit
 parseRedoubleLimit "unlimited" = Success Unlimited
 parseRedoubleLimit str = case reads str of
   [(num, "")] -> Success $ LimitedTo num
   _ -> Failure $ "unable to parse '" ++ str ++"' as redoubles limit"
+
+parseUTCTime :: String -> ParseResult UTCTime
+parseUTCTime str = case parseTime defaultTimeLocale "%s" str of
+  Just time -> Success time
+  Nothing -> Failure $ "unable to parse '" ++ str ++ "' as UTCTime"
 
--- test/CLIP.hs
+++ test/CLIP.hs
@@ -5,23 +5,69 @@ import Data.Time
 import System.Locale
 import Test.HUnit
 
-test_parseMessage_failedLoginParsedCorrectly = 
+test_failedLoginParsedCorrectly = 
   assertEqual "failed login" 
               (Success FailedLogin, []) 
               (parseMessage "login:")
 
-test_parseMessage_welcomeParsedCorrectly = 
+test_welcomeParsedCorrectly = 
   assertEqual "CLIP Welcome"
-              (Success (Welcome "username" (fromJust $ parseTime defaultTimeLocale "%s" "1041253132") "1.2.3.4"), [])
+              (Success (Welcome "username" (toUTCTime "1041253132") "1.2.3.4"), [])
               (parseMessage "1 username 1041253132 1.2.3.4")
 
-test_parseMessage_ownInfoParsedCorrectly = 
+test_ownInfoParsedCorrectly = 
   assertEqual "CLIP Own Info"
               (Success (OwnInfo "myself" True True False False False False True True 2396 False True False True 3457.85 False False (LimitedTo 0) False False "Australia/Melbourne"), [])
               (parseMessage "2 myself 1 1 0 0 0 0 1 1 2396 0 1 0 1 3457.85 0 0 0 0 0 Australia/Melbourne")
 
-test_parseMessages_isLazy = 
+test_motdParsedCorrectly = 
+  assertEqual "CLIP MOTD"
+              (Success (MOTD motd), [])
+              (parseMessage $ "3\r\n" ++ motd ++ "4\r\n")
+
+test_whoInfoParsedCorrectly = 
+  assertEqual "CLIP Who Info"
+              (Success (WhoInfo [
+                           (PlayerInfo "mgnu_advanced" (Just "someplayer") Nothing True False 1912.15 827 8 (toUTCTime "1040515752") "192.168.143.5" (Just "3DFiBs") Nothing),
+                           (PlayerInfo "someplayer" (Just "mgnu_advanced") Nothing False False 1418.61 23 1914 (toUTCTime "1041272421") "192.168.40.3" (Just "MacFIBS") (Just "someplayer@somewhere.com")),
+                           (PlayerInfo "anotherplayer" Nothing Nothing False False 1439.79 1262 410 (toUTCTime "1041251697") "somehost.com" Nothing Nothing)]), [])
+              (parseMessage whoInfoMsg)
+
+test_parseMessagesIsLazy = 
   assertEqual "parseMessages is lazy" 
               [Success FailedLogin, Success FailedLogin]
               (take 2 $ parseMessages $ cycle "login:")
 
+
+toUTCTime str = (fromJust $ parseTime defaultTimeLocale "%s" str)
+
+motd =
+  "+--------------------------------------------------------------------+\r\n\
+  \|                                                                    |\r\n\
+  \| It was a dark and stormy night in Oakland.  Outside, the rain      |\r\n\
+  \| came down in torrents.  Winds of 40MPH+ pounded at the windows,    |\r\n\
+  \| and whipped at trees, power lines, and anyone foolish enough       |\r\n\
+  \| to be outdoors.                                                    |\r\n\
+  \|                                                                    |\r\n\
+  \| In the middle of the night, I was awakened by a loud BEEP BEEP     |\r\n\
+  \| BEEP BEEP.  \"What is that?\" I thought groggily.  Ah!  We've had    |\r\n\
+  \| a power failure, and the computers are running on battery power.   |\r\n\
+  \|                                                                    |\r\n\
+  \| Curious to see how things were working, I crawled out of bed and   |\r\n\
+  \| stumbled downstairs to log in.  To my delight, people were         |\r\n\
+  \| merrily playing backgammon, oblivious to the fact that they had    |\r\n\
+  \| just ridden out a power failure that a few months ago would have   |\r\n\
+  \| shut down the server most ungracefully.                            |\r\n\
+  \|                                                                    |\r\n\
+  \| Thanks to all of the generous FIBSters who bought the UPS and      |\r\n\
+  \| made this possible!  And coming soon, as soon as I can get it      |\r\n\
+  \| built and deployed, a new (more reliable, and maybe faster)        |\r\n\
+  \| server.                                                            |\r\n\
+  \|                                                                    |\r\n\
+  \+--------------------------------------------------------------------+\r\n"
+
+whoInfoMsg = 
+  "5 mgnu_advanced someplayer - 1 0 1912.15 827 8 1040515752 192.168.143.5 3DFiBs -\r\n\
+  \5 someplayer mgnu_advanced - 0 0 1418.61 23 1914 1041272421 192.168.40.3 MacFIBS someplayer@somewhere.com\r\n\
+  \5 anotherplayer - - 0 0 1439.79 1262 410 1041251697 somehost.com - -\r\n\
+  \6\r\n"
