4fcbdf16d4aaa6439deec59dbffa29db36ddd7e0
Maciek Makowski <maciek.makowski@gmail.com>
2012-08-27 19:08:13 +0100

    set parent on match window

--- src/View/MatchWindow.hs
+++ src/View/MatchWindow.hs
@@ -3,6 +3,7 @@ where
 
 -- WX
 import Graphics.UI.WX 
+import Graphics.UI.WXCore.WxcDefs
 
 import Backgammon (initialBoard)
 import View.Board
@@ -11,12 +12,13 @@ startWidth, startHeight :: Int
 startWidth = 800
 startHeight = 800
 
-createMatchWindow :: Frame () -> IO (Frame ())
+createMatchWindow :: Frame a -> IO (Frame ())
 createMatchWindow f = do 
-  mw <- frame [ text := "Match" 
-              -- TODO: , parent := f 
-              ]
+  fw <- get f frameParent
+  mw <- frameEx wxDEFAULT_FRAME_STYLE [ text := "Match" ] fw
   p <- panel mw [on paint := paintBoard initialBoard]
-  set mw [ layout := minsize (sz startWidth startHeight) $ fill $ widget p ]
-
+  set mw [ layout := minsize (sz startWidth startHeight) $ fill $ widget p
+         , on resize := do newSize <- get mw clientSize
+                           set p [outerSize := newSize]
+                           repaint p]
   return mw
